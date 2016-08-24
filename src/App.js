2bbf5fa48df2bd6788195c2033b238bc610c40f3
Keith Wade <keawade@gmail.com>
2016-08-24 08:09:44 -0500

    Start adding state

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import History from './components/History';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: []
+    }
+  }
   render() {
     return (
       <div className="App">
         <Search />
-        <Viewer />
-        <History />
+        <Viewer current={this.state.list[0]} />
+        <History list={this.state.list} />
       </div>
     );
   }
